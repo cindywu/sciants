ebc14218ccbe2791464adbc5b917c1ef5d9e5259
Tim Tan <tim.j.tan@gmail.com>
2012-06-23 16:43:56 -0700

    links

--- index.php
+++ index.php
@@ -4,5 +4,10 @@
 	</head>
 	<body>
 		<img src='http://1.bp.blogspot.com/-j_pM7Uwfs8Y/T3ZcN2WVfvI/AAAAAAAAARk/0hG01BsvNUw/s1600/%2386+-+Forgiveness.png' alt='fuckyou'>
+		
+		<a href="http://sciants.co/projectsplash.php">Project Splash</a>
+		<a href="http://sciants.co/project.php">Project</a>
+		<a href="http://sciants.co/note.php">Note</a>
+		
 	</body>
 </html>
