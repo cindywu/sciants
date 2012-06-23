931e6687f2cb8b1e7470d019ac2c20321900e1f0
denny <denny@denny-vb.(none)>
2012-06-23 15:39:25 -0700

    third try

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 		<title>SciAnts</title>
 	</head>
 	<body>
-		<img src='http://i1.kym-cdn.com/entries/icons/original/000/004/457/challenge.jpg' />
-		<?php phpinfo(); ?>
+		<img src='http://i1.kym-cdn.com/entries/icons/original/000/004/457/challenge.jpg'>
 	</body>
 </html>
