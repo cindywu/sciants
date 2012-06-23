cbc8d9fa616457fba31e66e98e630b4de85e486f
denny <denny@denny-vb.(none)>
2012-06-23 16:15:52 -0700

    comic

--- index.php
+++ index.php
@@ -3,8 +3,6 @@
 		<title>SciAnts</title>
 	</head>
 	<body>
-		<img src='http://i1.kym-cdn.com/entries/icons/original/000/004/457/challenge.jpg' alt='fuckyou'>
-		<img src='http://s-ak.buzzfed.com/static/enhanced/terminal01/2010/7/27/15/enhanced-buzz-25036-1280257450-8.jpg' alt='tapir'>
-		<img src='http://weknowmemes.com/wp-content/uploads/2012/01/oh-stop-it-you.png' alt='cute'>
+		<img src='http://1.bp.blogspot.com/-j_pM7Uwfs8Y/T3ZcN2WVfvI/AAAAAAAAARk/0hG01BsvNUw/s1600/%2386+-+Forgiveness.png' alt='fuckyou'>
 	</body>
 </html>
