03b6e64988a8f3e6c6b0e7d2f11be163d29f6800
denny <denny@denny-vb.(none)>
2012-06-23 15:36:58 -0700

    oops, fixed quote

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 		<title>SciAnts</title>
 	</head>
 	<body>
-		<img src=http://i1.kym-cdn.com/entries/icons/original/000/004/457/challenge.jpg>
+		<img src='http://i1.kym-cdn.com/entries/icons/original/000/004/457/challenge.jpg' />
 		<?php phpinfo(); ?>
 	</body>
 </html>
