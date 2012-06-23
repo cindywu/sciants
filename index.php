00b6f86c97c34670b890c22f2988c09ef994ed88
denny <denny@denny-vb.(none)>
2012-06-23 15:43:01 -0700

    alt text

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 		<title>SciAnts</title>
 	</head>
 	<body>
-		<img src='http://i1.kym-cdn.com/entries/icons/original/000/004/457/challenge.jpg'>
+		<img src='http://i1.kym-cdn.com/entries/icons/original/000/004/457/challenge.jpg' alt='fuckyou'>
 	</body>
 </html>
