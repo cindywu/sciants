734d8747b54d2e83e520b64fa9bece3d033dc548
denny <denny@denny-vb.(none)>
2012-06-23 16:07:55 -0700

    tapir

--- index.php
+++ index.php
@@ -4,5 +4,7 @@
 	</head>
 	<body>
 		<img src='http://i1.kym-cdn.com/entries/icons/original/000/004/457/challenge.jpg' alt='fuckyou'>
+		<img src='http://s-ak.buzzfed.com/static/enhanced/terminal01/2010/7/27/15/enhanced-buzz-25036-1280257450-8.jpg' alt='tapir'>
+		<img src='http://weknowmemes.com/wp-content/uploads/2012/01/oh-stop-it-you.png' alt='cute'>
 	</body>
 </html>
