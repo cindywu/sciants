8b0d17404e6c1f940f5956aeaeb58f656a8d650e
Tim Tan <tim.j.tan@gmail.com>
2012-06-23 20:09:45 -0700

    wat

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
 	</head>
 	
 	<body>
-		<img src='http://1.bp.blogspot.com/-j_pM7Uwfs8Y/T3ZcN2WVfvI/AAAAAAAAARk/0hG01BsvNUw/s1600/%2386+-+Forgiveness.png' alt='fuckyou'>
 		
 		<a href="http://sciants.co/projectssplash.php">Project Splash</a>
 		<a href="http://sciants.co/project.php">Project</a>
