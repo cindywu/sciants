fd1f76152351b3b1211d57e0a106ec3b41989a1f
seanjosephwu <seanjosephwu@gmail.com>
2012-06-24 12:20:48 -0700

    Updating

--- pages/project.php
+++ pages/project.php
@@ -54,12 +54,6 @@
 				$fullNote = $noteStore->getNote($authToken, $note->guid, true, false, false, false);
 			?>
 				<p>Note?: <?=$fullNote->content?></p>
-			<?php
-			}
-			foreach ($notes as $note) {
-				$fullNote = $noteStore->getNote($authToken, $note->guid, true, false, false, false);
-			?>
-				<p>Note?: <?=$fullNote->content?></p>
 				<a href="http://www.sciants.co/pages/notes.html">
 					<div class="notes">
 						<div class="notes_date">6/20</div>
