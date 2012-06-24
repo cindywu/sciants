847eb1283db7b3ade1a60f571af153327fe75335
seanjosephwu <seanjosephwu@gmail.com>
2012-06-23 21:12:22 -0700

    More PHP testing

--- pages/evernotetest.php
+++ pages/evernotetest.php
@@ -107,7 +107,7 @@ $notebooks = $noteStore->listNotebooks($authToken);
 	foreach ($notebooks as $notebook) {
 		$notefilter = new NoteFilter();
 		$notefilter->notebookGuid = $notebook->guid;
-		$allnotes = noteStore.findNotes($authToken, $notefilter, 0, 100);
+		$allnotes = $noteStore.findNotes($authToken, $notefilter, 0, 100);
 		//$notebookname = $notebook->name;
 		//foreach ($notes->notes as $note) {
 	}
