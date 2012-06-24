1b7ceae204e97dab02a3f49127e057d13dd8b72a
Tim Tan <tim.j.tan@gmail.com>
2012-06-23 17:04:06 -0700

    favicon

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <html>
 	<head>
 		<title>SciAnts</title>
+		<link rel="icon" type="image/png" href="http://sciants.co/favicon.png">
 	</head>
+	
 	<body>
 		<img src='http://1.bp.blogspot.com/-j_pM7Uwfs8Y/T3ZcN2WVfvI/AAAAAAAAARk/0hG01BsvNUw/s1600/%2386+-+Forgiveness.png' alt='fuckyou'>
 		
-		<a href="http://sciants.co/projectsplash.php">Project Splash</a>
+		<a href="http://sciants.co/projectssplash.php">Project Splash</a>
 		<a href="http://sciants.co/project.php">Project</a>
 		<a href="http://sciants.co/note.php">Note</a>
 		
